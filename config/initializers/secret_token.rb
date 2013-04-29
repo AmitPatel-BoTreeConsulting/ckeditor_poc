eed42422edb667d3ce45fc05212847c8e50ed095
AmitPatel-BoTreeConsulting <amit.patel@botreeconsulting.com>
2013-04-29 14:50:40 +0530

    initial commit comment

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CkeditorPoc::Application.config.secret_token = 'aea5b410abd5b5c3e1b849102e0719e5d8ecf315ea8f1506f82d7738e200daf1a399caa2aaeb199d06f2935b6fcd59850810e0cfd425ebda0a67c305c4fd4f3a'
