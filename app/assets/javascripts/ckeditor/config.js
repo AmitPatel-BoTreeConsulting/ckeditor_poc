5731dbe3643d41d4be10e0123004a17ba4764782
AmitPatel-BoTreeConsulting <amit.patel@botreeconsulting.com>
2013-04-30 10:39:36 +0530

    Configure toolbar

---
+++ app/assets/javascripts/ckeditor/config.js
@@ -0,0 +1,9 @@
+//http://docs.cksource.com/CKEditor_3.x/Developers_Guide/Toolbar
+//http://docs.exponentcms.org/docs/2.0.9/ckeditor-toolbar-configuration
+//https://github.com/galetahub/ckeditor/issues/239
+CKEDITOR.config.toolbar_mini =
+    [
+        ['Bold','Italic','Underline'],
+        ['NumberedList','BulletedList'],
+        ['Link','Unlink']
+    ];
